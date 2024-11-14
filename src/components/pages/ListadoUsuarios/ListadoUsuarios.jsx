135908e8e2ee49b2c9a2d3c11b812c4b7671d8bf
Admabaga <admabaga@outlook.com>
2024-11-14 16:51:07 -0500

    Refactor modificando listado usuarios

--- src/components/pages/ListadoUsuarios/ListadoUsuarios.jsx
+++ src/components/pages/ListadoUsuarios/ListadoUsuarios.jsx
@@ -1,52 +1,61 @@
 import { useEffect, useState } from "react"
 
-export default function ListadoUsuarios(){
+export default function ListadoUsuarios() {
     //dataFake
     const dataFake = [
         {
             id: 1,
             nombre: 'Juan',
-            telefono: '871616912', 
-            ciudad: 'Medellin'},
+            telefono: '871616912',
+            ciudad: 'Medellin'
+        },
         {
             id: 2,
-            nombre: 'Andres', 
-            telefono: '321', 
-            ciudad: 'Medellin'},
+            nombre: 'Andres',
+            telefono: '321',
+            ciudad: 'Medellin'
+        },
         {
             id: 3,
-            nombre: 'Felipe', 
-            telefono: '151561', 
-            ciudad: 'Medellin'},
+            nombre: 'Felipe',
+            telefono: '151561',
+            ciudad: 'Medellin'
+        },
         {
             id: 4,
-            nombre: 'Julian', 
-            telefono: '12345648', 
-            ciudad: 'Medellin'}
+            nombre: 'Julian',
+            telefono: '12345648',
+            ciudad: 'Medellin'
+        }
     ]
-    return(
+
+    const [datosApi, setDatosApi] = useState(null);
+    const [estadoCarga, setEstadoCarga] = useState(true);
+    // Programo el use effect para garantizar que llamara el servicio y voy a traer los datos
+    useEffect(() => {
+        const fetchData = async () => {
+            setDatosApi(dataFake);
+            setEstadoCarga(false);
+        };
+        fetchData();
+    }, []);
+    return (
         <>
-        <br /><br /><br />  
-        {}
-        <h3>Listado de Usuarios</h3>
-        <table>
-            <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Telefono</th>
-                    <th>Ciudad</th>
-                </tr>
-            </thead>
-            <tbody>
-                {dataFake.map((usuario) => (
-                    <tr key={usuario.id}>
-                        <td>{usuario.nombre}</td>
-                        <td>{usuario.telefono}</td>
-                        <td>{usuario.ciudad}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+            <br /><br /><br />
+            { }
+            <h3>Listado de Usuarios</h3>
+            <div className="container">
+                <div className="row row-cols-1 row-cols-md-3 g-3">
+                    {datosApi && datosApi.map(usuario => (
+                        <div className="col" key={usuario.id}>
+                            <div className="card h-100 shadow p-5">
+                                <h5>Nombre: {usuario.nombre}</h5>
+                                <h4>Ciudad: {usuario.ciudad}</h4>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
 
         </>
     )
