67f818840fe0fa703e36b141decffbf4d43aa901
LeonDavid-ctrl <david.mejia.leon421@gmail.com>
2024-11-20 09:58:25 -0500

    Feat modificando spinner

--- src/components/pages/ListadoUsuarios/ListadoUsuarios.jsx
+++ src/components/pages/ListadoUsuarios/ListadoUsuarios.jsx
@@ -1,62 +1,76 @@
-import { useEffect, useState } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function ListadoUsuarios() {
-    //dataFake
-    const dataFake = [
-        {
-            id: 1,
-            nombre: 'Juan',
-            telefono: '871616912',
-            ciudad: 'Medellin'
-        },
-        {
-            id: 2,
-            nombre: 'Andres',
-            telefono: '321',
-            ciudad: 'Medellin'
-        },
-        {
-            id: 3,
-            nombre: 'Felipe',
-            telefono: '151561',
-            ciudad: 'Medellin'
-        },
-        {
-            id: 4,
-            nombre: 'Julian',
-            telefono: '12345648',
-            ciudad: 'Medellin'
-        }
-    ]
+  const [datosApi, setDatosApi] = useState(null);
+  const [respuestaServer, setRespuestaServer] = useState("");
+  const [respuestaError, setRespuestaError] = useState(false);
+  const [spinner, setSpinner] = useState(false);
 
-    const [datosApi, setDatosApi] = useState(null);
-    const [estadoCarga, setEstadoCarga] = useState(true);
-    // Programo el use effect para garantizar que llamara el servicio y voy a traer los datos
-    useEffect(() => {
-        const fetchData = async () => {
-            setDatosApi(dataFake);
-            setEstadoCarga(false);
-        };
-        fetchData();
-    }, []);
-    return (
-        <>
-            <br /><br /><br />
-            { }
-            <h3>Listado de Usuarios</h3>
-            <div className="container">
-                <div className="row row-cols-1 row-cols-md-3 g-3">
-                    {datosApi && datosApi.map(usuario => (
-                        <div className="col" key={usuario.id}>
-                            <div className="card h-100 shadow p-5">
-                                <h5>Nombre: {usuario.nombre}</h5>
-                                <h4>Ciudad: {usuario.ciudad}</h4>
-                            </div>
-                        </div>
-                    ))}
+  useEffect(() => {
+    const fetchData = async () => {
+      setSpinner(true);
+      try {
+        const respuesta = await axios.get("http://localhost:8000/usuarios");
+        setDatosApi(respuesta.data);
+        setRespuestaError(false);
+      } catch (error) {
+        setRespuestaServer(`Error al enviar datos: ${error.message}`);
+        setRespuestaError(true);
+      } finally {
+        setSpinner(false);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <>
+      <br />
+      <br />
+      <br />
+      <div className="container">
+        <h3>Listado de Usuarios</h3>
+        <div className="row row-cols-1 row-cols-md-3 g-3">
+          {datosApi &&
+            datosApi.map((usuario) => (
+              <div className="col" key={usuario.id}>
+                <div className="card h-100 shadow p-5">
+                  <h4>Nombre: {usuario.nombre}</h4>
+                  <h5>Ciudad: {usuario.ciudad}</h5>
+                  <h5>Telefono: {usuario.telefono}</h5>
                 </div>
-            </div>
-
-        </>
-    )
+              </div>
+            ))}
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        {spinner ? (
+          <div className="d-flex justify-content-center ">
+            <div className="spinner-border"></div>
+            <span className="m-1"> Cargando...</span>
+          </div>
+        ) : (
+          <>
+            {respuestaServer && (
+              <p
+                className={`respuestaServer ${
+                  respuestaError ? "error" : "success"
+                }`}
+              >
+                {respuestaServer}
+              </p>
+            )}
+          </>
+        )}
+      </div>
+    </>
+  );
 }
--- src/components/pages/MetodoPago/MetodoPago.jsx
+++ src/components/pages/MetodoPago/MetodoPago.jsx
@@ -4,7 +4,6 @@ export default function MetodoPago(){
     return(
         <>
             <FormularioMetodoPago></FormularioMetodoPago>
-
         </>
     )
 }
